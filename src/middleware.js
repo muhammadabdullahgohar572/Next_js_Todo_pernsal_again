deb904a595108de1b895ac8ebe1384768f31e4b0
Abdullah <muhammadabdullahgohar572@gmail.com>
2025-06-22 04:08:17 +0500

    first commit

--- src/middleware.js
+++ src/middleware.js
@@ -2,15 +2,14 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
   const jwt = request.cookies.get("logintoken")?.value;
-
+ const { pathname } = request.nextUrl;
   if (request.nextUrl.pathname.startsWith("/api")) {
     return NextResponse.next();
   }
 
   const Auth =
-    request.nextUrl.pathname === "/pages/SignUp" ||
-    request.nextUrl.pathname === "pages/Login";
-
+     pathname === "/pages/SignUp" ||
+    pathname === "/pages/Login"; 
   if (jwt && Auth) {
     return NextResponse.redirect(new URL("/pages/add_Task", request.url));
   }
